5eff31a17601999f5734c55eabb388002e55c7ae
ambill <ambill@users.noreply.github.com>
2015-07-16 17:00:45 +0800

    Revert "在“修业查看”中显示全部课程GPA和平均分"
    This reverts commit 3278b8e14b98154030c409e564024fcb8f9ed60c.

--- content_script/eductionList.js
+++ content_script/eductionList.js
@@ -1,55 +1,34 @@
-gpa_global ={
-    "bixiu": 0,
-    "xianxuan": 0,
-    "tongshi": 0,
-    "gexinghua": 0,
-    "tiyu": 0,
-    "liangke": 0,
-    "units": 0,
-    "grades": 0,
-    "scores": 0
-};
-
 function optimize_myEductionList(){
     if(!inUrl("/edu/StuStatusMange/MyGradList.aspx"))
         return 0;
 
-    gpa_global["units"] = 0;
-    gpa_global["grades"] = 0;
-    gpa_global["scores"] = 0;
-    gpa_global["bixiu"] = gpa_colomn("#dgBX1", "#lbBxxf2", [6,5,7]); //必修
-    gpa_global["tiyu"] = gpa_colomn("#dgPE", "#lbPE1", [6,5,7]); //体育
-    gpa_global["liangke"] = gpa_colomn("#dgTH", "#lbTH1", [6,5,7]); //两课
-    gpa_global["xianxuan"] = gpa_colomn("#dgXX2", "#lbXxxf1", [7,6,8]); //限选
-    gpa_global["tongshi"] = gpa_colomn("#dgTsk2", "#lbTSK1", [7,6,9]); //通识
-    gpa_global["gexinghua"] = gpa_colomn("#dgGxh2", "#lbGxxf1", [6,5,7]); //任选
+    gpa_colomn("#dgBX1", "#lbBxxf2", [6,5,7]); //必修
+    gpa_colomn("#dgPE", "#lbPE1", [6,5,7]); //体育
+    gpa_colomn("#dgTH", "#lbTH1", [6,5,7]); //两课
+    gpa_colomn("#dgXX2", "#lbXxxf1", [7,6,8]); //限选
+    gpa_colomn("#dgTsk2", "#lbTSK1", [7,6,9]); //通识
+    gpa_colomn("#dgGxh2", "#lbGxxf1", [6,5,7]); //任选
 
-    // 将统计结果显示在最上方的总表格中
-    var p = "共修" + gpa_global["units"] + "学分, 平均分为" + (gpa_global["scores"]/gpa_global["units"]).toFixed(2) + "，平均GPA为" + (gpa_global["grades"]/gpa_global["units"]).toFixed(2);
-    jQuery("#form1 > table > tbody > tr:nth-child(6)").after("<tr><td><fieldset style='width:900px'><legend>所有课程分数情况（由electsys++提供）</legend><table width='100%'><p>"+p+"</p></table></fieldset></td></tr>");
 }
 
 function gpa_colomn(table, title, colomn){
     /**
         @table: 成绩表格的id
         @title: 标题
-        @colomn: 代表“成绩”，“学分”，“备注”所在列
+        @colomn: 每个表格的“成绩”等所在列不一样，用colomn表示取那一列
     */
     var gpa_all = 0;
     var unit_all = 0;
     var score_all = 0;
     jQuery(table).find("tr").each(function(){
         var score = jQuery(this).find("td:nth-child(" + colomn[0] +")").html();
-        var unit = parseInt(jQuery(this).find("td:nth-child(" + colomn[1] +")").html());
+        var unit = jQuery(this).find("td:nth-child(" + colomn[1] +")").html();
         var comment = jQuery(this).find("td:nth-child(" + colomn[2] +")").html();
         var gpa = score2gpa(score);
         if (typeof(gpa) == "number" && comment != "无需关注" && comment != "尚未修读"){
-            gpa_all += gpa * unit;
-            unit_all += unit;
-            score_all += score * unit;
-            gpa_global["scores"] += score * unit;
-            gpa_global["units"] += unit;
-            gpa_global["grades"] += gpa * unit;
+            gpa_all += gpa * parseInt(unit);
+            unit_all += parseInt(unit);
+            score_all += score * parseInt(unit);
         }
         if (comment == "无需关注" || comment == "尚未修读"){
             gpa = comment;
@@ -58,7 +37,7 @@ function gpa_colomn(table, title, colomn){
     });
     var gpa_avg = gpa_all / unit_all;
     var score_avg = score_all / unit_all;
-    jQuery(title).parent().append("本部分课程总GPA为：" + gpa_avg.toFixed(2) + "，平均分为：" + score_avg.toFixed(2));
+    jQuery(title).parent().append("。 必修课总GPA为：" + gpa_avg.toFixed(2) + "，平均分为：" + score_avg.toFixed(2));
 }
 
 function score2gpa(score){
